32881aedf3c39380b978057ab726230416b0e7ed
juanmana <juanmana0402@gmail.com>
2020-08-18 11:33:20 +0200

    probando

---
+++ docs/precache-manifest.d2e2df467594891ae50c7d484671a431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6130429cc2eff81000ae27fa154264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8408522f3128544c6de2",
+    "url": "/static/css/main.d98afe06.chunk.css"
+  },
+  {
+    "revision": "cec30beedda70fa405d7",
+    "url": "/static/js/2.e881aa95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e881aa95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8408522f3128544c6de2",
+    "url": "/static/js/main.62d32b0d.chunk.js"
+  },
+  {
+    "revision": "6a26a6a45f9e65ecd26f",
+    "url": "/static/js/runtime-main.0ba3af82.js"
+  }
+]);
